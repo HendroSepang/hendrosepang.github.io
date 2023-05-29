4e42e5400066621f683b2cbf8e4fee1844920528
HendroSepang <96098097+HendroSepang@users.noreply.github.com>
2023-05-29 23:56:42 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function turnOn() {
+    var lightBulb = document.getElementById('light-bulb');
+    lightBulb.classList.add('on');
+  }
+  
+  function turnOff() {
+    var lightBulb = document.getElementById('light-bulb');
+    lightBulb.classList.remove('on');
+  }
+  
